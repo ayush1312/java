65fbd5d8e97ee47ef450fa7cd1ebe48eb27e3579
ayush1312 <ayush.abhishek13@gmail.com>
2019-08-30 10:20:56 +0530

    commit gradle project

---
+++ src/test/java/helloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class helloWorldTest {
+
+    @Test
+    public void testMain(){
+        helloWorld hw = new helloWorld();
+        hw.main(null);
+    }
+
+}
